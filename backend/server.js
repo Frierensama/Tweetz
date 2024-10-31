8519ba68b7ccafa22fcb3b533050a5e598a27872
ski <rudeusgreyrat.zeta002@gmail.com>
2024-10-31 23:58:37 +0530

    user routes created

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,26 @@
 import express from "express";
-import authRoutes from "./routes/auth.routes.js";
 import dotenv from 'dotenv';
-import { connect } from "mongoose";
-import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
+
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js"
+
+import connectMongoDB from "./db/connectMongoDB.js";
 const app = express();
 
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT  || 2000 ;
+
 
 dotenv.config();
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 
 app.use(cookieParser())
 
@@ -17,6 +28,7 @@ app.use(express.json()) //to parse req.body
 app.use(express.urlencoded({extended:true})) //to parse form data
 
 app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes)
 
 app.listen(PORT,()=>{
     console.log("server started");
