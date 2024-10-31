aac5aea750ce85483ead4059a00ab2d22a675eca
ski <rudeusgreyrat.zeta002@gmail.com>
2024-10-31 16:56:10 +0530

    Protected Routes
    for getme to view user data for the user.

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
-
+import { login, logout, signup ,getMe } from "../controllers/auth.controller.js";
+import protectRoute from "../middleware/protectRoute.js"
 
 const Router = express.Router();
 
+
+Router.get("/me",protectRoute , getMe)
 Router.route("/signup").post(signup);
-Router.get('/login',login );
-Router.get('/logout',logout);
+Router.route("/login").post(login);
+Router.route("/logout").post(logout);
 
 export default Router;
 
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,15 @@ import authRoutes from "./routes/auth.routes.js";
 import dotenv from 'dotenv';
 import { connect } from "mongoose";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 const PORT = process.env.PORT || 2000;
 
 dotenv.config();
+
+app.use(cookieParser())
 
 app.use(express.json()) //to parse req.body
 app.use(express.urlencoded({extended:true})) //to parse form data
